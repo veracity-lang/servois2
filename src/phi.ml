7877272659a10cc09f2c204b0c00cb3e2433e4c0
jaredpincus <jpincus@stevens.edu>
2021-12-16 18:13:14 -0500

    Phi -> Smt conversion

--- src/phi.ml
+++ src/phi.ml
@@ -1,8 +1,14 @@
-type atom = Smt.exp (* TODO: narrow this to just the kinds of atoms we want *)
+open Smt
+
+type atom = exp (* TODO: narrow this to just the kinds of atoms we want *)
 
 type conjunction =
   Conj of atom list
 
 type t = Disj of conjunction list
 
-(* TODO: SMT expr. of conjunction and disjunction *)
+let smt_of_conj (Conj al) =
+  ELop (And, al)
+
+let smt_of_phi (Disj cl) =
+  ELop (Or, (List.map smt_of_conj cl))
