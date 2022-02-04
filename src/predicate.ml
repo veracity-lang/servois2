8dc153d9f8f45832d15a2b8e922d1f8b5cc356ae
parisa f <p16_f3@yahoo.com>
2022-02-04 13:56:08 -0500

    fix an issue in predicate generator

--- src/predicate.ml
+++ src/predicate.ml
@@ -2,39 +2,10 @@ open Spec
 open Smt
 
 
-let string_of_binop = function
-  | Mul    -> "*"
-  | Div    -> "/" (* TODO: I think smtlib2 actually uses "div"? *)
-  | Sub    -> "-"
-  | Lt     -> "<"
-  | Lte    -> "<="
-  | Gt     -> ">"
-  | Gte    -> ">="
-  | Eq     -> "=="
-  | Mod    -> "mod"
-  | Abs    -> "abs"
-  | Imp    -> "=>"
-
-let string_of_lop = function
-  | Add -> "+"
-  | And -> "&&"
-  | Or -> "||"
-
-let rec string_of_exp (e: exp) =
-  match e with
-  | EVar (Var str) -> str
-  | EVar (VarPost str) -> str 
-  | EConst (CInt i) -> Int.to_string i
-  | EConst (CBool b) -> Bool.to_string b
-  | EBop (bop,exp1,exp2) -> "(" ^ string_of_binop bop ^ " "^ string_of_exp exp1 ^ " "^ string_of_exp exp2 ^ ")" 
-  | ELop (lop, [exp1; exp2]) ->  "(" ^ string_of_lop lop ^ " "^ string_of_exp exp1 ^ " "^ string_of_exp exp2 ^ ")" 
-  | _ -> "other exp"
-
-
 let is_reflx (op: string) (exp1: exp) (exp2: exp) : bool =
   match op with
   | "=" -> 
-    if String.equal (string_of_exp exp1) (string_of_exp exp2) then
+    if String.equal (Smt_ToMLString.exp exp1) (Smt_ToMLString.exp exp2) then
       true
     else 
       false
@@ -46,8 +17,8 @@ let is_symm (op: string) (exp1: exp) (exp2: exp) =
   match op with
   | "=" -> 
     List.exists (fun (o,e1,e2) -> 
-        (String.equal o op) && (String.equal (string_of_exp exp1) (string_of_exp e2)) 
-        && (String.equal (string_of_exp exp2) (string_of_exp e1))
+        (String.equal o op) && (String.equal (Smt_ToMLString.exp exp1) (Smt_ToMLString.exp e2)) 
+        && (String.equal (Smt_ToMLString.exp exp2) (Smt_ToMLString.exp e1))
     ) preds_list
 
   | _ -> false
@@ -77,6 +48,9 @@ let generate_predicates (spec: spec) (method1: method_spec) (method2: method_spe
 
   let pred_list = ref [] in
   List.iter (fun [@warning "-8"] (PredSig (name,[ty1;ty2])) ->
+    match (Hashtbl.find_opt all_terms ty1), (Hashtbl.find_opt all_terms ty2) with
+    | None, _ | _, None -> ()
+    | Some ty1_terms, Some ty2_terms ->  
     List.iter (
       fun left ->
       List.iter (
@@ -92,4 +66,3 @@ let generate_predicates (spec: spec) (method1: method_spec) (method2: method_spe
   (* List.iter (fun p -> Printf.printf "-> Preds: %s\n" (string_of_pred p)) !pred_list; *)
 
   !pred_list
-
