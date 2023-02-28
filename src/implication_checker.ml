447950b62bf0ea1488591ea9a190ad52d48b9580
Mihai N <lnicola@stevens.edu>
2023-02-28 02:37:27 -0500

    Add new implication checking rule and bug fixing

--- src/implication_checker.ml
+++ src/implication_checker.ml
@@ -40,26 +40,50 @@ let rp7: rpred = (7, Gt, ["x"; "y"], ["n"])
 let rp8: rpred = (8, Gt, ["x"; "y"], ["n"])   
 (* x - y = 0 *)
 let rp9: rpred = (9, Eq, ["x"; "y"], [])
-(* x >= m *)
-let rp10: rpred = (10, Gte, ["x"], ["m"])
 (* x >= n *)
-let rp11: rpred = (11, Gte, ["x"], ["n"])
-(* x > m *)
-let rp12: rpred = (12, Gt, ["x"], ["m"])
+let rp10: rpred = (10, Gte, ["x"], ["n"])
 (* x > n *)
-let rp13: rpred = (13, Gt, ["x"], ["n"])
-(* x < m *)
-let rp14: rpred = (14, Lt, ["x"], ["m"])
+let rp12: rpred = (12, Gt, ["x"], ["n"])
 (* x < n *)
-let rp15: rpred = (15, Lt, ["x"], ["n"])
-(* x <= m *)
-let rp16: rpred = (16, Lte, ["x"], ["m"])
+let rp14: rpred = (14, Lt, ["x"], ["n"])
 (* x <= n *)
-let rp17: rpred = (17, Lte, ["x"], ["n"])
+let rp16: rpred = (16, Lte, ["x"], ["n"])
+(* x + y > y | y + x > y*)
+let rp18: rpred = (18, Gt, ["x"], [])
+(* x > 0 *)
+let rp19: rpred = (19, Gt, ["x"], [])
+(* x - z > y - z *)
+let rp20: rpred = (20, Gt, ["x"; "y"], [])
+(* x + y = y | y + x = y *)
+let rp21: rpred = (21, Eq, ["x"], [])
+(* x = 0 *)
+let rp22: rpred = (22, Eq, ["x"], [])
+
+(* not (x > y) *)
+let rp23: rpred = (23, Lte, ["x"; "y"], []) 
+(* not (x + z > y + z) *)
+let rp24: rpred = (24, Lte, ["x"; "y"], [])
+(* not (x - y = 0) *)
+let rp25: rpred = (25, Neq, ["x"; "y"], [])
+(* not (x - y > n) *)
+let rp26: rpred = (26, Lte, ["x"; "y"], ["n"])
+(* not (x > y + n) *)
+let rp27: rpred = (27, Lte, ["x"; "y"], ["n"]) 
+(* not (x - z > y - z) *)
+let rp28: rpred = (28, Lte, ["x"; "y"], [])
+(* not (x + y > y) | not (y + x > y) *)
+let rp29: rpred = (29, Lte, ["x"], [])
+(* not (x > 0) *)
+let rp30: rpred = (30, Lte, ["x"], [])
+(* not (x + y = y) | not (y + x = y) *)
+let rp31: rpred = (31, Neq, ["x"], [])
+(* not (x = 0) *)
+let rp32: rpred = (32, Neq, ["x"], [])
 
 (* list of predicates in premises or conclusions of rules *)
 let rule_preds = [rp1; rp2; rp3; rp4; rp5; rp6; rp7; rp8; rp9; 
-                  rp10; rp11; rp12; rp13; rp14; rp15; rp16; rp17 ]
+                  rp10; rp12; rp14; rp16; rp18; rp19; rp20; rp21; rp22;
+                  rp23; rp24; rp25; rp26; rp27; rp28; rp29; rp30; rp31; rp32 ]
 
 (* rules are defined as ordered pairs of predicates + a hypothesis *)
 let defined_rules: rules = [
@@ -74,10 +98,31 @@ let defined_rules: rules = [
   rp3, rp1, (function [m;n] -> m <= n | _ -> false); (* x + m > y + n   =>  x > y, when m < n *)
   rp9, rp4, empty_rhypothesis;                       (* x - y = 0  =>  x = y   *)
   rp4, rp9, empty_rhypothesis;                       (* x = y  =>  x - y = 0   *)
-  rp10, rp11, (function [m;n] -> m >= n | _ -> false);(* x >= m => x >= n, when m >= n *)
-  rp12, rp13, (function [m;n] -> m >= n | _ -> false);(* x > m => x > n, when m >= n *)
-  rp14, rp15, (function [m;n] -> m <= n | _ -> false);(* x < m => x < n, when m <= n *)
-  rp16, rp17, (function [m;n] -> m <= n | _ -> false) (* x <= m => x <= n, when m <= n *)
+  rp10, rp10, (function [n1;n2] -> n1 >= n2 | _ -> false);(* x >= n1 => x >= n2, when n1 >= n2 *)
+  rp12, rp12, (function [n1;n2] -> n1 >= n2 | _ -> false);(* x > n1 => x > n2, when n1 >= n2 *)
+  rp12, rp10, (function [n1;n2] -> n1 >= n2 | _ -> false);(* x > n1 => x >= n2, when n1 >= n2 *)
+  rp14, rp14, (function [n1;n2] -> n1 <= n2 | _ -> false);(* x < n1 => x < n2, when n1 <= n2 *)
+  rp16, rp16, (function [n1;n2] -> n1 <= n2 | _ -> false);(* x <= n1 => x <= n2, when n1 <= n2 *)
+  rp14, rp16, (function [n1;n2] -> n1 <= n2 | _ -> false);(* x < n1 => x <= n2, when n1 <= n2 *)
+  rp18, rp19, empty_rhypothesis;                     (* x + y > y => x > 0 *)
+  rp19, rp18, empty_rhypothesis;                     (* x > 0 => x + y > y *)
+  rp20, rp1, empty_rhypothesis;                      (* x - z > y - z  => x > y *)
+  rp1, rp20, empty_rhypothesis;                      (* x > y => x - z > y - z *)
+  rp21, rp22, empty_rhypothesis;                     (* x + y = y => x = 0 *)
+  rp22, rp21, empty_rhypothesis;                     (* x = 0 => x + y = y *)
+  
+  rp23, rp24, empty_rhypothesis;                     (* not (x > y) => not (x + z > y + z) *)
+  rp24, rp23, empty_rhypothesis;                     (* not (x + z > y + z) => not (x > y) *)
+  rp25, rp5, empty_rhypothesis;                      (* not (x - y = 0) => not (x = y) *)
+  rp5, rp25, empty_rhypothesis;                      (* not (x = y) => not (x - y = 0) *)
+  rp26, rp27, empty_rhypothesis;                     (* not (x - y > n) => not (x > y + n) *)
+  rp27, rp26, empty_rhypothesis;                     (* not (x > y + n) => not (x - y > n) *)
+  rp23, rp28, empty_rhypothesis;                     (* not (x > y) => not (x - z > y - z) *)
+  rp28, rp23, empty_rhypothesis;                     (* not (x - z > y - z) => not (x > y) *)
+  rp29, rp30, empty_rhypothesis;                     (* not (x + y > y) => not (x > 0) *)
+  rp30, rp29, empty_rhypothesis;                     (* not (x > 0) => not (x + y > y) *)
+  rp31, rp32, empty_rhypothesis;                     (* not (x + y = y) => not (x = 0) *)
+  rp32, rp31, empty_rhypothesis;                     (* not (x = 0) => not (x + y = y) *)
 ]
 
 (* To compute the transitive closure of the implication relation under the set of 
@@ -100,7 +145,9 @@ let trans_clos: rpred list -> rules -> (rpred * (rpred * rhypothesis) list) list
           else add rs tl (premises rs rpa ((rpa, hypos)::acc))
         | [] -> acc
       in
-      let tc = List.map (fun p -> (p, remove_self p @@ premises rs p [])) ps in
+      let reflp p = List.find_all (fun (rpa, rpb, hyp) -> rpa == rpb && rpa == p) rs |> 
+                          List.map (fun (rpa, _, hyp) -> (rpa, hyp)) in  
+      let tc = List.map (fun p -> (p, (remove_self p @@ premises rs p []) @ (reflp p))) ps in
       pfvv "\n Transitive Closure: \n%s"
         (String.concat "\n" (List.map (fun (q, ps) -> 
              Printf.sprintf "(q: %s, premises: [%s])"
@@ -143,6 +190,8 @@ let trans_clos: rpred list -> rules -> (rpred * (rpred * rhypothesis) list) list
  *)
 let _check_implication: rpred list -> rules -> Smt.predP * Smt.predP -> bool = 
   fun rps rs (p,q) ->
+  let fvarIdGen = ref 0 in
+  let new_fvar m = fvarIdGen := !fvarIdGen + 1; m ^ (string_of_int !fvarIdGen) in
   let rec canonical_predP = function
     | P (">", EConst e1, e2) -> P ("<", e2, EConst e1)
     | P (">=", EConst e1, e2) -> P ("<=", e2, EConst e1)
@@ -182,14 +231,36 @@ let _check_implication: rpred list -> rules -> Smt.predP * Smt.predP -> bool =
       (8, Gt, [x;y], [n]) ->
       [(x, e1); (y, e2); (n, EConst e3)]
     | P ("=", EBop(Sub, e1, e2), EConst(CInt 0)), (9, Eq, [x;y], []) -> [(x, e1); (y, e2)]
-    | (P (">=", e1, EConst e2_v), (10, Gte, [x], [m]) 
-      | P (">=", e1, EConst e2_v), (11, Gte, [x], [m])) -> [(x, e1); (m, EConst e2_v)]
-    | (P (">", e1, EConst e2_v), (12, Gte, [x], [m]) 
-      | P (">", e1, EConst e2_v), (13, Gte, [x], [m])) -> [(x, e1); (m, EConst e2_v)]
-    | (P ("<", e1, EConst e2_v), (14, Gte, [x], [m]) 
-      | P ("<", e1, EConst e2_v), (15, Gte, [x], [m]))-> [(x, e1); (m, EConst e2_v)]
-    | (P ("<=", e1, EConst e2_v), (16, Gte, [x], [m]) 
-      | P ("<=", e1, EConst e2_v), (17, Gte, [x], [m]))-> [(x, e1); (m, EConst e2_v)]
+    | P (">=", e1, EConst e2_v), (10, Gte, [x], [m]) -> [(x, e1); (new_fvar m, EConst e2_v)]
+    | P (">", e1, EConst e2_v), (12, Gt, [x], [m]) -> [(x, e1); (new_fvar m, EConst e2_v)]
+    | P ("<", e1, EConst e2_v), (14, Lt, [x], [m]) -> [(x, e1); (new_fvar m, EConst e2_v)]
+    | P ("<=", e1, EConst e2_v), (16, Lte, [x], [m]) -> [(x, e1); (new_fvar m, EConst e2_v)]
+    | (P (">", ELop(Add, [e1; e2]), e3), (18, Gt, [x], [])) -> 
+      if e1 = e3 then [(x, e2)] else if e2 = e3 then [(x, e1)] else []
+    | (P (">", e1, EConst(CInt 0)), (19, Gt, [x], [])) -> [(x, e1)]
+    | (P (">", EBop(Sub, e1, e2), EBop(Sub, e3, e4)), (20, Gt, [x;y], [])) ->
+       if e2 = e4 then [(x, e1); (y, e3)] else []
+    | (P ("=", ELop(Add, [e1; e2]), e3), (21, Eq, [x], [])) -> 
+      if e1 = e3 then [(x, e2)] else if e2 = e3 then [(x, e1)] else []
+    | (P ("=", e1, EConst(CInt 0)), (22, Eq, [x], [])) -> [(x, e1)]           
+    | P ("<=", e1, e2), (23, Lte, [x;y], []) -> [(x, e1); (y, e2)]
+    | P ("<=", ELop(Add, [e1; e2]), ELop(Add, [e3; e4])), (24, Lte, [x;y], _) -> 
+      if e2 = e4 then [(x, e1); (y, e3)] else []
+    | NotP ("=", EBop(Sub, e1, e2), EConst(CInt 0)), (25, Neq, [x; y], []) -> 
+      [(x, e1); (y, e2)]
+    | P ("<=", EBop(Sub, e1, e2), EConst e3), (26, Lte, [x;y], []) -> 
+      [(x, e1); (y, e2)]
+    | P ("<=", e1, ELop(Add, [e2; EConst e3])), (27, Lte, [x;y], []) -> 
+      [(x, e1); (y, e2)]
+    | P ("<=", EBop(Sub, e1, e2), EBop(Sub, e3, e4)), (28, Lte, [x;y], []) -> 
+      if e2 = e4 then [(x, e1); (y, e3)] else []
+    | P ("<=", ELop(Add, [e1; e2]), e3), (29, Lte, [x], []) -> 
+      if e1 = e3 then [(x, e2)] else if e2 = e3 then [(x, e1)] else []
+    | P ("<=", e1, EConst(CInt 0)), (30, Lte, [x], []) -> [(x, e1)]
+    | NotP ("=", ELop(Add, [e1; e2]), e3), (31, Eq, [x], []) -> 
+      if e1 = e3 then [(x, e2)] else if e2 = e3 then [(x, e1)] else []
+    | NotP ("=", e1, EConst(CInt 0)), (32, Eq, [x], []) -> [(x, e1)]
+                                                            
     | q, rp -> pfvv "\nUnmatched: %s | %s" (string_of_predP q) (string_of_rpred rp); []
   in
   let rec check_prms rp_q s = function
@@ -205,21 +276,24 @@ let _check_implication: rpred list -> rules -> Smt.predP * Smt.predP -> bool =
       let rec agree_on_q_bvars: substitution -> substitution -> string list -> bool = 
         fun s s' bvars ->
           match bvars with
-          | [] -> true
+          | [] -> pfvv "\nBvars agreement"; true
           | bvar::tl -> 
             begin match List.assoc_opt bvar s, List.assoc_opt bvar s' with
-              | Some x, Some x' -> if x = x' then agree_on_q_bvars s s' tl else false
+              | Some x, Some x' -> if x = x' then let () = pfvv "\nAgree on bvar" in agree_on_q_bvars s s' tl else false
               | _, _ -> false
             end
       in
       let rec collect_fterms s acc = function
         | [] -> acc
         | fvar::tl -> 
-          match List.assoc_opt fvar s with
-          | Some (EConst(CInt v)) -> collect_fterms s (v::acc) tl
+          pfvv "\nFVar: %s" fvar;
+          match List.find_opt (fun (m, _) -> (String.sub m 0 (String.length fvar)) = fvar) s with
+          | Some (fvar_m, EConst(CInt v)) -> 
+            pfvv "\nFVar %s -> %d" fvar_m v; collect_fterms s (v::acc) tl
           | _ -> collect_fterms s acc tl
       in 
       let fterms = collect_fterms s' (collect_fterms s [] (List.rev q_fvars)) (List.rev p_fvars) in
+      pfvv "\nFreeVar Terms: %s" (String.concat "; " (List.map string_of_int fterms));
       let rhypos_holds = hypos fterms in 
       if (agree_on_q_bvars s s' q_bvars) && rhypos_holds then true else check_prms rp_q s tl
       
